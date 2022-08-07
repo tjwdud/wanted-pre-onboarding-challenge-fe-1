d3749ad6633d42323a50f15d6284f4fe7c7dd17c
sjy <stseo012@gmail.com>
2022-08-08 01:57:18 +0900

    feat: 로그인 & 회원가입 폼

---
+++ src/pages/auth/index.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+
+function Auth() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [submitDisabled, setSubmitDisabled] = useState(true);
+
+  const onChangeEmailHandler = ({ target: { value } }) => setEmail(value);
+  const onChangePasswordHandler = ({ target: { value } }) => setPassword(value);
+  const onClickLoginHandler = async (e) => {
+    e.preventDefault();
+  };
+  const onClickSignInHandler = async (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div>
+      <form>
+        <div>
+          <input
+            name="email"
+            type="email"
+            placeholder="이메일"
+            value={email}
+            onChange={onChangeEmailHandler}
+          />
+        </div>
+        <div>
+          <input
+            name="password"
+            type="password"
+            placeholder="비밀번호"
+            value={password}
+            onChange={onChangePasswordHandler}
+          />
+        </div>
+        <div>
+          <button onClick={onClickLoginHandler} disabled={submitDisabled}>
+            로그인
+          </button>
+          <button onClick={onClickSignInHandler} disabled={submitDisabled}>
+            회원가입
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Auth;
